d7c3776f63a7406717b1dd844c2fc8a90e1621ba
leoneBellotti <leone.bellotti@gmail.com>
2024-04-18 10:32:21 -0300

    commit 2

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import cors from "cors";
 
 const backURL = `${import.meta.env.VITE_API_URL}`;
 
 window.backURL = backURL;
 
 const app = createApp(App);
+
+app.use(cors({
+    origin:'https://cartoola.onrender.com/',
+    credentials:true
+}));
 
 app.use(router);
 
